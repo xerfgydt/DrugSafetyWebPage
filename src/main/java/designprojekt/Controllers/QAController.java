c2fee08bb73df67a423d7df354a601caaff28f8d
MEBL94 <mebl94@gmail.com>
2018-05-12 11:57:14 +0200

    added some styling to the menu

--- src/main/java/designprojekt/Controllers/QAController.java
+++ src/main/java/designprojekt/Controllers/QAController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class QAController {
 
-    @RequestMapping
+    @RequestMapping("/qa")
     public String QA(Model model) {
         return "qa";
     }
