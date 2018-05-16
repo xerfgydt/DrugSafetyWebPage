73ae041099b6d97b99bdb66583937f6d38341e4b
MEBL94 <mebl94@gmail.com>
2018-05-16 12:56:26 +0200

    changed index controller and renamed index in menu to home

--- src/main/java/designprojekt/Controllers/IndexController.java
+++ src/main/java/designprojekt/Controllers/IndexController.java
@@ -2,11 +2,12 @@ package designprojekt.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public String index() {
         return "index";
     }
